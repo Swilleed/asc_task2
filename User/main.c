23feec9afefc1ce0ee79d1646507994ca451c8e0
Swilleed <willwupy@outlook.com>
2025-11-01 20:15:23 +0800

    前一版timer干扰电机正常运行（TIM2定时器冲突），修复了这一问题，编码器和电机可以正常工作

--- User/main.c
+++ User/main.c
@@ -3,23 +3,35 @@
 #include "OLED.h"
 #include "Motor.h"
 #include "Key.h"
+#include "Encoder.h"
+#include "Timer.h"
+#include "pid.h"
+#include "Delay.h"
 
 uint8_t KeyNum;
 int8_t Speed1, Speed2;
 int8_t CurrentSpeed1, CurrentSpeed2;
 uint8_t statu = 0;
-uint64_t EncoderCount1 = 0;
-uint64_t EncoderCount2 = 0;
+int64_t EncoderCount1 = 0;
+int64_t EncoderCount2 = 0;
 
 int main(void)
 {
     OLED_Init();
     Motor_Init();
     Key_Init();
+    Encoder_Init();
+    Timer_Init();
 
     OLED_ShowString(1, 1, "Speed:");
 
     while (1) {
+        OLED_ShowNum(1, 13, EncoderCount1, 5);
+        OLED_ShowNum(2, 13, EncoderCount2, 5);
+        OLED_ShowString(1, 1, "Speed:");
+        OLED_ShowNum(1, 7, Speed1, 3);
+        OLED_ShowNum(2, 7, Speed2, 3);
+
         KeyNum = Key_GetNum();
         if (statu == 0) {
             Motor_SetSpeed(20, 100);
@@ -32,13 +44,13 @@ int main(void)
     }
 }
 
-void TIM2_IRQHandler(void)
+void TIM1_UP_IRQHandler(void)
 {
-    if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET) {
+    if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET) {
         Speed1 = Encoder1_Get();
         Speed2 = Encoder2_Get();
         EncoderCount1 += Speed1;
         EncoderCount2 += Speed2;
-        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+        TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
     }
 }
