f9143bc759236b1dd3893859bd24e6287cb44afd
Swilleed <willwupy@outlook.com>
2025-11-01 15:51:04 +0800

    双电机驱动

--- Hardware/Motor.c
+++ Hardware/Motor.c
@@ -1,31 +1,36 @@
-#include "stm32f10x.h"                  // Device header
+#include "stm32f10x.h" // Device header
 #include "PWM.h"
 
 void Motor_Init(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-	
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-	
-	PWM_Init();
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    PWM_Init();
 }
 
 void Motor_SetSpeed(int8_t Speed)
 {
-	if (Speed >= 0)
-	{
-		GPIO_SetBits(GPIOB, GPIO_Pin_12);
-		GPIO_ResetBits(GPIOB, GPIO_Pin_13);
-		PWM_SetCompare3(Speed);
-	}
-	else
-	{
-		GPIO_ResetBits(GPIOB, GPIO_Pin_12);
-		GPIO_SetBits(GPIOB, GPIO_Pin_13);
-		PWM_SetCompare3(-Speed);
-	}
+    if (Speed >= 0) {
+        GPIO_SetBits(GPIOB, GPIO_Pin_12);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+        PWM_SetCompare3(Speed);
+        GPIO_SetBits(GPIOB, GPIO_Pin_14);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_15);
+        PWM_SetCompare4(Speed);
+    }
+    else {
+        GPIO_ResetBits(GPIOB, GPIO_Pin_12);
+        GPIO_SetBits(GPIOB, GPIO_Pin_13);
+        PWM_SetCompare3(-Speed);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_14);
+        GPIO_SetBits(GPIOB, GPIO_Pin_15);
+        PWM_SetCompare4(-Speed);
+    }
 }
